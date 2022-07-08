11b2ab8580959fb822bc4bee1e8f26bca985e4af
cristiano800 <107884089+cristiano800@users.noreply.github.com>
2022-07-08 15:57:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,19 +32,19 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.owner = ['212695290310'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['212695290310'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['212695290310'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'ᴏᴡᴇɴ ʙᴏᴛ' // Your name ඔබේ නම 
+global.botnma = 'ᴏᴡᴇɴ ʙᴏᴛ' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = 'ᴍᴏᴜᴀᴅ ᴇʟ' //ownernama,ownername
+global.packname = 'ᴍʏ ɪɴsᴛᴀɢʀᴀᴍ :  ᴍᴏᴜᴜᴀᴅ__ᴇʟᴋ' // Sticker package name 
+global.author = 'ᴏᴡᴇɴ ʙᴏᴛ' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
 // Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
-global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
+global.alivelogo = `https://telegra.ph/file/7536fe34d87f2daf7dc17.jpg`
 global.alive =`Hello i am alive now
 
 Download song type .yt 
